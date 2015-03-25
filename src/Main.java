47beef4904e69a21e023387e89e15145b15ff6cb
Isabella Rocha <isabellaral@gmail.com>
2015-03-25 20:13:56 -0300

    estruturando o código

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,27 @@
 import java.util.Scanner;
 
 public class Main {
+    static int response = -1;
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Saiba quantas rodas tem o seu automóvel. Digite:\n1 para carro\n2 para moto\nResposta: ");
-        getInput();
-    }
-
-    private static void getInput() {
-        Scanner scanner = new Scanner(System.in);
-        String response = scanner.nextLine();
-        processResponse(response);
-    }
-
-    private static void processResponse(String response) {
-        if (response.equals("1")) {
-            sendMessageAboutCar();
-        } else if (response.equals("2")) {
-            sendMessageAboutMotorcycle();
-        } else {
-            sendNotAValidOptionMessage(response);
+        while (response != 0) {
+            getUserInput();
+            if (response == 1) {
+                System.out.println("\nA quantidade de rodas do carro é 4\n\n");
+            } else if (response == 2) {
+                System.out.println("\nA quantidade de rodas da moto é 2\n\n");
+            } else if (response == 9) {
+                System.out.println("Você está finalizando o programa!");
+                break;
+            } else {
+                System.out.printf(String.format("A opção %s não é válida. Por favor, tente novamente!\n\n", response));
+            }
         }
-        main(null);
     }
 
-    private static void sendNotAValidOptionMessage(String response) {
-        System.out.printf(String.format("A opção %s não é válida. Por favor, tente novamente!\n\n", response));
-    }
-
-    private static void sendMessageAboutMotorcycle() {
-        System.out.println("\nA quantidade de rodas da moto é 2\n\n");
-    }
-
-    private static void sendMessageAboutCar() {
-        System.out.println("\nA quantidade de rodas do carro é 4\n\n");
+    static void getUserInput() {
+        System.out.println("Saiba quantas rodas tem o seu automóvel. Digite:\n1 para carro\n2 para moto\n9 para sair\nResposta: ");
+        response = scanner.nextInt();
     }
 }
